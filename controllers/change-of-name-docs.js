04fcee895b4e7bb436f408c12d08639e6a640171
Adrian <a.brereton@kainos.com>
2018-01-05 10:08:38 +0000

    move accordion to other changes page only

--- controllers/change-of-name-docs.js
+++ controllers/change-of-name-docs.js
@@ -14,15 +14,15 @@ Controller.prototype.get = function successHandler(req, res, callback) {
 	} else {
 	switch (req.sessionModel.get('change-of-name-reason')) {
 		case 'Marriage-or-civil-partnership':
-			return res.redirect('./name-change-docs#marriage')
+			return res.redirect('./name-change-docs-for-marriage')
 		case 'Divorce':
-			return res.redirect('./name-change-docs#divorce')
+			return res.redirect('./name-change-docs-for-divorce')
 		case 'Small':
-			return res.redirect('./name-change-docs#first-name')
+			return res.redirect('./name-change-docs-for-small-changes')
 		case 'Gender-reassigment':
-			return res.redirect('./name-change-docs#gender')
+			return res.redirect('./name-change-docs-for-gender-change')
 		case 'I-changed-my-name-another-way':
-			return res.redirect('./name-change-docs#other')
+			return res.redirect('./name-change-docs-for-other-changes')
 	}
 	}
 }
